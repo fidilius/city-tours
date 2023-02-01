7200963aeb7143b7e63b60c53423cf0d7b46d773
fidilius <fidilius.siregar@gmail.com>
2023-02-02 01:33:23 +0700

    data: fix name & add city

--- backend/data.js
+++ backend/data.js
@@ -2,25 +2,29 @@ const data = [
   {
     id: 1,
     img: process.env.BASE_URL + "/img/tokyo.jpg",
-    name: "Tokyo",
+    city: "Tokyo",
+    name: "tokyo sushi tour",
     info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, ipsum. Itaque sed dolores ullam vel sunt. Velit fugit cumque inventore?",
   },
   {
     id: 2,
     img: process.env.BASE_URL + "/img/paris.jpg",
-    name: "Paris",
+    city: "Paris",
+    name: "paris light tours",
     info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, ipsum. Itaque sed dolores ullam vel sunt. Velit fugit cumque inventore?",
   },
   {
     id: 3,
     img: process.env.BASE_URL + "/img/london.jpg",
-    name: "London",
+    city: "London",
+    name: "london royal palace tour",
     info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, ipsum. Itaque sed dolores ullam vel sunt. Velit fugit cumque inventore?",
   },
   {
     id: 4,
     img: process.env.BASE_URL + "/img/new-york.jpg",
-    name: "New York",
+    city: "New York",
+    name: "new york bridge tour",
     info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, ipsum. Itaque sed dolores ullam vel sunt. Velit fugit cumque inventore?",
   },
 ];
